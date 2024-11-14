9e54bb2f761efd4af4895d1bb3b81ed9752367a4
neyond <neyond@gmail.com>
2024-11-14 16:12:59 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,18 +12,22 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Workouts Map',
-  siteUrl: 'https://ben29.xyz',
-  logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
+  siteUrl: 'https://go.eimoo.com',
+  logo: 'https://eimoo.com/service/avatar/0ab4d0eec14aa1dc634d6d75dffe80d9',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://ben29.xyz',
+      url: 'https://eimoo.com',
     },
     {
       name: 'About',
-      url: 'https://github.com/ben-29/workouts_page/blob/master/README-CN.md',
+      url: 'https://eimoo.com/about',
+    },
+    {
+      name: 'Strava',
+      url: 'https://www.strava.com/athletes/yangli',
     },
   ],
 };
